86bd559359f121cb4f1b5761c9da1e3488c56fbf
ibrahim Lahlou <ibrahim.lahlou021@gmail.com>
2023-07-31 19:10:51 +0100

    testing the bad UX it gave

--- ping_pong_game.js
+++ ping_pong_game.js
@@ -3,7 +3,8 @@ let playerScore = 0, aiScore = 0;
 let ballSpeedX, ballSpeedY;
 const PADDLE_WIDTH = 15, PADDLE_HEIGHT = 100;
 const BALL_WIDTH = 20, BALL_HEIGHT = 20;
-const PADDLE_SPEED = 7, BALL_SPEED_X = 7, BALL_SPEED_Y = 7;
+const PADDLE_SPEED = 7, BALL_SPEED_X = 1, BALL_SPEED_Y = 1;
+let gameStarted = false;
 
 function setup() {
   createCanvas(800, 600);
@@ -19,62 +20,100 @@ function setup() {
 function draw() {
   background(255);
 
-  // Draw paddles and ball
-  fill(0);
-  rect(playerPaddle.x, playerPaddle.y, PADDLE_WIDTH, PADDLE_HEIGHT);
-  rect(aiPaddle.x, aiPaddle.y, PADDLE_WIDTH, PADDLE_HEIGHT);
-  ellipse(ball.x + BALL_WIDTH / 2, ball.y + BALL_HEIGHT / 2, BALL_WIDTH, BALL_HEIGHT);
+  if (gameStarted) {
+    if (!isPaused) {
+      // Draw paddles and ball
+      fill(0);
+      rect(playerPaddle.x, playerPaddle.y, PADDLE_WIDTH, PADDLE_HEIGHT);
+      rect(aiPaddle.x, aiPaddle.y, PADDLE_WIDTH, PADDLE_HEIGHT);
+      ellipse(ball.x + BALL_WIDTH / 2, ball.y + BALL_HEIGHT / 2, BALL_WIDTH, BALL_HEIGHT);
 
-  // Move paddles and ball
-  if (keyIsDown(87) && playerPaddle.y > 0) {
-    playerPaddle.y -= PADDLE_SPEED;
-  }
-  if (keyIsDown(83) && playerPaddle.y < height - PADDLE_HEIGHT) {
-    playerPaddle.y += PADDLE_SPEED;
-  }
+      // Move paddles and ball
+      if (keyIsDown(87) && playerPaddle.y > 0) {
+        playerPaddle.y -= PADDLE_SPEED;
+      }
+      if (keyIsDown(83) && playerPaddle.y < height - PADDLE_HEIGHT) {
+        playerPaddle.y += PADDLE_SPEED;
+      }
 
-  if (ball.y < 0 || ball.y > height - BALL_HEIGHT) {
-    ballSpeedY *= -1;
-  }
+      if (ball.y < 0 || ball.y > height - BALL_HEIGHT) {
+        ballSpeedY *= -1;
+      }
 
-  // AI movement
-  if (ball.y + BALL_HEIGHT / 2 < aiPaddle.y + PADDLE_HEIGHT / 2) {
-    aiPaddle.y -= PADDLE_SPEED;
-  }
-  if (ball.y + BALL_HEIGHT / 2 > aiPaddle.y + PADDLE_HEIGHT / 2) {
-    aiPaddle.y += PADDLE_SPEED;
-  }
+      // AI movement
+      if (ball.y + BALL_HEIGHT / 2 < aiPaddle.y + PADDLE_HEIGHT / 2) {
+        aiPaddle.y -= PADDLE_SPEED;
+      }
+      if (ball.y + BALL_HEIGHT / 2 > aiPaddle.y + PADDLE_HEIGHT / 2) {
+        aiPaddle.y += PADDLE_SPEED;
+      }
 
-  ball.x += ballSpeedX;
-  ball.y += ballSpeedY;
+      ball.x += ballSpeedX;
+      ball.y += ballSpeedY;
 
-  // Ball collision with paddles
-  if (ball.x < playerPaddle.x + PADDLE_WIDTH && ball.x + BALL_WIDTH > playerPaddle.x &&
-    ball.y < playerPaddle.y + PADDLE_HEIGHT && ball.y + BALL_HEIGHT > playerPaddle.y) {
-    ballSpeedX *= -1;
-  }
+      // Ball collision with paddles
+      if (ball.x < playerPaddle.x + PADDLE_WIDTH && ball.x + BALL_WIDTH > playerPaddle.x &&
+        ball.y < playerPaddle.y + PADDLE_HEIGHT && ball.y + BALL_HEIGHT > playerPaddle.y) {
+        ballSpeedX *= -1;
+      }
 
-  if (ball.x < aiPaddle.x + PADDLE_WIDTH && ball.x + BALL_WIDTH > aiPaddle.x &&
-    ball.y < aiPaddle.y + PADDLE_HEIGHT && ball.y + BALL_HEIGHT > aiPaddle.y) {
-    ballSpeedX *= -1;
-  }
+      if (ball.x < aiPaddle.x + PADDLE_WIDTH && ball.x + BALL_WIDTH > aiPaddle.x &&
+        ball.y < aiPaddle.y + PADDLE_HEIGHT && ball.y + BALL_HEIGHT > aiPaddle.y) {
+        ballSpeedX *= -1;
+      }
 
-  // Ball goes out of bounds
-  if (ball.x < 0 || ball.x > width) {
-    if (ball.x < 0) {
-      aiScore++;
+      // Ball goes out of bounds
+      if (ball.x < 0 || ball.x > width) {
+        if (ball.x < 0) {
+          aiScore++;
+        } else {
+          playerScore++;
+        }
+
+        ballSpeedX = BALL_SPEED_X * (random() < 0.5 ? 1 : -1);
+        ballSpeedY = BALL_SPEED_Y * (random() < 0.5 ? 1 : -1);
+        ball = createVector(width / 2 - BALL_WIDTH / 2, height / 2 - BALL_HEIGHT / 2);
+      }
+
+      // Display scores
+      fill(0);
+      textSize(40);
+      text(`Player: ${playerScore}`, 20, 40);
+      text(`AI: ${aiScore}`, width - 120, 40);
     } else {
-      playerScore++;
+      // Game is paused, display countdown timer
+      fill(0);
+      textSize(24);
+      textAlign(CENTER);
+      let countdown = Math.ceil((pauseEndTime - millis()) / 1000);
+      text(`Game Paused - Resume in ${countdown} seconds`, width / 2, height / 2);
     }
-
-    ballSpeedX = BALL_SPEED_X * (random() < 0.5 ? 1 : -1);
-    ballSpeedY = BALL_SPEED_Y * (random() < 0.5 ? 1 : -1);
-    ball = createVector(width / 2 - BALL_WIDTH / 2, height / 2 - BALL_HEIGHT / 2);
+  } else {
+    // Display start button
+    fill(0);
+    textSize(24);
+    textAlign(CENTER);
+    text('Click Start Game to Play', width / 2, height / 2);
   }
+}
 
-  // Display scores
-  fill(0);
-  textSize(40);
-  text(`Player: ${playerScore}`, 20, 40);
-  text(`AI: ${aiScore}`, width - 120, 40);
+function startGame() {
+  gameStarted = true;
+  let startButton = select('#startButton');
+  startButton.hide();
+}
+
+function pauseGame() {
+  if (!isPaused) {
+    isPaused = true;
+    let pauseButton = select('#pauseButton');
+    pauseButton.html('Resume Game');
+    pauseEndTime = millis() + pauseDuration;
+    noLoop();
+  } else {
+    isPaused = false;
+    let pauseButton = select('#pauseButton');
+    pauseButton.html('Pause Game');
+    loop();
+  }
 }
